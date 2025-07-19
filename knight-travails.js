edc4be50ca1d1fdf02adc10bb4a83d993551bdad
golam71 <74001687+golam71@users.noreply.github.com>
2025-07-19 14:28:22 +0600

    ♻️ Refactor knightMoves function for improved readability and efficiency

--- knight-travails.js
+++ knight-travails.js
@@ -9,29 +9,34 @@ function knightMoves(start, end) {
     [-1, 2],
     [-1, -2],
   ];
+
   const isValid = (x, y) => x >= 0 && x < 8 && y >= 0 && y < 8;
 
   const queue = [[start]];
-  const visited = new Set();
-  visited.add(`${start[0]},${start[1]}`);
+  const visited = new Set([`${start[0]},${start[1]}`]);
 
   while (queue.length > 0) {
     const path = queue.shift();
     const [x, y] = path.at(-1);
+
     if (x === end[0] && y === end[1]) {
+      console.log(
+        `=> You made it in ${path.length - 1} moves!  Here's your path:`,
+      );
+      path.forEach((p) => console.log(`\t[${p[0]}, ${p[1]}]`));
       return path;
     }
-    for (const [dx, dy] of KNIGHT_MOVES) {
-      const newX = x + dx;
-      const newY = y + dy;
-      const position = `${newX},${newY}`;
 
-      if (isValid(newX, newY) && !visited.has(position)) {
-        visited.add(position);
+    for (const [dx, dy] of KNIGHT_MOVES) {
+      const [newX, newY] = [x + dx, y + dy];
+      const posKey = `${newX},${newY}`;
+
+      if (isValid(newX, newY) && !visited.has(posKey)) {
+        visited.add(posKey);
         queue.push([...path, [newX, newY]]);
       }
     }
   }
 }
 
-console.log(knightMoves([3, 3], [4, 3]));
+knightMoves([3, 3], [4, 3]);
